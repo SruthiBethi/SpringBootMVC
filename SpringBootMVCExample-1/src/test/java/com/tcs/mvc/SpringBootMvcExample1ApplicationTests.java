8456a700aeb938d9d17c8085c41aafa04a0b1637
SruthiBethi <bethisruthi35@gmail.com>
2024-12-25 17:28:25 +0530

    first commit

---
+++ SpringBootMVCExample-1/src/test/java/com/tcs/mvc/SpringBootMvcExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
